38890e70117673ed7a3833df474c617991438fdc
Felix Anneblad <felix.anneblad@gmail.com>
2023-01-05 11:54:07 +0100

    changes in code

--- counter.js
+++ counter.js
@@ -1,17 +1,3 @@
-//document.getElementById("count-el").innerText = 5;
-
-// let lap1 = 32
-// let lap2 = 31
-// let lap3 = 31
-// function laptimes(){
-//     console.log(`lap 1 has a time of ${lap1} seconds!`)
-//     console.log(`lap 2 has a time of ${lap2} seconds!`)
-//     console.log(`lap 3 has a time of ${lap3} seconds!`)
-// }
-
-// laptimes();
-/*-------------------------------------------------------------------------*/
-//document.getElementById("count-el").innerText = ;
 let countEl = document.getElementById("count-el")
 let count = 0
 
